a5624d6b4ea2c8db4ccb06c32e0f749ba4c27504
Aaron Bojarski <aaron.bojarski@empa.ch>
2023-05-15 11:26:18 +0200

    update renku version add workflow file

---
+++ python/src/main.py
@@ -0,0 +1,22 @@
+import sys
+
+
+"""
+This script is a demonstration for how to use renku workflows.
+The workflow itself is defined in the workflow.yml file. It can be executed within a renku environment with 
+
+renku run workflow.yml
+
+More information can be found here https://renku.readthedocs.io/en/latest/topic-guides/workflows/workflow-file.html
+"""
+
+
+if __name__ == "__main__":
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    with open(input_file, "r") as f:
+        txt = f.read()
+
+    with open(output_file, "w") as f:
+        f.write(txt)
